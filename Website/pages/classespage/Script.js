50d78e8f07e972af186861bfdfce6b46a3f03370
Priestly Patrick Bassey <Pbassey30@gmail.com>
2022-11-23 13:09:05 +0100

    Front-end project
    Commit

---
+++ Website/pages/classespage/Script.js
@@ -0,0 +1,48 @@
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModel')
+    }
+});
+
+
+// Get the modal
+var modal2 = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
